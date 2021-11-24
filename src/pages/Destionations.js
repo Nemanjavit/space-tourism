c7571169891c0e3e7ff8150ea68c841ecb94b112
Nemanjavit <vitnemanja3@gmail.com>
2021-11-23 21:56:16 -0600

    added animations

--- src/pages/Destionations.js
+++ src/pages/Destionations.js
@@ -33,44 +33,67 @@ function Destionations() {
   console.log(destinations);
   return (
     <div className="destPage">
-      <div className="glide">
-        <div className="glide__track" data-glide-el="track">
-          <ul className="glide__slides">
-            {destinations.map((destination, i) => {
-              return (
-                <li key={i} className="glide__slide">
-                  <h2 className="destPage__heading">
-                    <span className="destPage__heading__number">01</span>
-                    PICK YOUR DESTINATION
-                  </h2>
-                  <div className="destPage__imageWrapper">
-                    <img className="destPage__image" src={getImage(i)} />
-                  </div>
-                  <div className="glide__bullets" data-glide-el="controls[nav]">
-                    <button className="glide__bullet" data-glide-dir="=0">
-                      MOON
-                    </button>
-                    <button className="glide__bullet" data-glide-dir="=1">
-                      MARS
-                    </button>
-                    <button className="glide__bullet" data-glide-dir="=2">
-                      EUROPA
-                    </button>
-                    <button className="glide__bullet" data-glide-dir="=3">
-                      TITAN
-                    </button>
-                  </div>
-                  <h1>{destination.name.toUpperCase()}</h1>
-                  <p>{destination.description}</p>
-                  <hr />
-                  <h3>AVG.DISTANCE</h3>
-                  <h2>{destination.distance}</h2>
-                  <h3>EST.TRAVEL TIME</h3>
-                  <h2>{destination.travel}</h2>
-                </li>
-              );
-            })}
-          </ul>
+      <div className="wrapper">
+        <div className="glide">
+          <div className="glide__track" data-glide-el="track">
+            <ul className="glide__slides">
+              {destinations.map((destination, i) => {
+                return (
+                  <li key={i} className="glide__slide">
+                    <h2 className="destPage__heading">
+                      <span className="destPage__heading__number">01</span>
+                      PICK YOUR DESTINATION
+                    </h2>
+                    <div className="destPage__imageWrapper">
+                      <img className="destPage__image" src={getImage(i)} />
+                    </div>
+                    <div
+                      className="glide__bullets sliderButtons__wrapper"
+                      data-glide-el="controls[nav]"
+                    >
+                      <button
+                        className="glide__bullet sliderButton "
+                        data-glide-dir="=0"
+                      >
+                        MOON
+                      </button>
+                      <button
+                        className="glide__bullet sliderButton"
+                        data-glide-dir="=1"
+                      >
+                        MARS
+                      </button>
+                      <button
+                        className="glide__bullet sliderButton"
+                        data-glide-dir="=2"
+                      >
+                        EUROPA
+                      </button>
+                      <button
+                        className="glide__bullet sliderButton"
+                        data-glide-dir="=3"
+                      >
+                        TITAN
+                      </button>
+                    </div>
+                    <div className="destPage__destInfo">
+                      <h1 className="destPage__destName">
+                        {destination.name.toUpperCase()}
+                      </h1>
+                      <p className="destPage__destDescription">
+                        {destination.description}
+                      </p>
+                      <hr className="destPage__hr" />
+                      <h4>AVG. DISTANCE</h4>
+                      <h3>{destination.distance.toUpperCase()}</h3>
+                      <h4>EST. TRAVEL TIME</h4>
+                      <h3>{destination.travel.toUpperCase()}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
