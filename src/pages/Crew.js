3d87f0a4fde3b07482fb97c930d0315ff6572b8e
Nemanjavit <vitnemanja3@gmail.com>
2021-11-24 21:38:28 -0600

    added crew page

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,11 +1,93 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+// glide
+import "@glidejs/glide/src/assets/sass/glide.core.scss";
+import Glide from "@glidejs/glide";
+
+import { getData } from "../Api/Api";
+// images
+import crew_0 from "../assets/crew/image-douglas-hurley.png";
+import crew_1 from "../assets/crew/image-mark-shuttleworth.png";
+import crew_2 from "../assets/crew/image-victor-glover.png";
+import crew_3 from "../assets/crew/image-anousheh-ansari.png";
 
 function Crew() {
-    return (
-        <div>
-            <h2>Crew</h2>
+  const [crew, setCrew] = useState([]);
+
+  let glide;
+
+  useEffect(() => {
+    getData()
+      .then((res) => {
+        setCrew(res.data.crew);
+      })
+      .then(() => glide.mount());
+    glide = new Glide(".glide", {
+      perView: 1,
+      type: "carousel",
+    });
+  }, []);
+  const getImage = (i) => {
+    let array = [crew_0, crew_1, crew_2, crew_3];
+    return array[i];
+  };
+  console.log(crew);
+  return (
+    <div className="crewPage">
+      <div className="wrapper">
+        <h2>Crew</h2>
+        <div className="glide">
+          <div className="glide__track" data-glide-el="track">
+            <ul className="glide__slides">
+              {crew.map((crewmember, i) => {
+                return (
+                  <li className="glide__slide" key={i}>
+                    <h4>
+                      <span>02</span>MEET YOUR CREW
+                    </h4>
+                    <div className="crewPage__crewmember">
+                      <div className="crewPage__info">
+                        <h3>{crewmember.role}</h3>
+                        <h2>{crewmember.name}</h2>
+                        <p className="crewPage__bio">{crewmember.bio}</p>
+                      </div>
+                      <div
+                        className="glide__bullets"
+                        data-glide-el="controls[nav]"
+                      >
+                        <button
+                          className="glide__bullet circleBullets "
+                          data-glide-dir="=0"
+                        ></button>
+                        <button
+                          className="glide__bullet circleBullets "
+                          data-glide-dir="=1"
+                        ></button>
+                        <button
+                          className="glide__bullet circleBullets"
+                          data-glide-dir="=2"
+                        ></button>
+                        <button
+                          className="glide__bullet circleBullets"
+                          data-glide-dir="=3"
+                        ></button>
+                      </div>
+                      <div className="crewPage__imageWrapper">
+                        <img
+                          className="crewPage__image"
+                          src={getImage(i)}
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Crew
+export default Crew;
